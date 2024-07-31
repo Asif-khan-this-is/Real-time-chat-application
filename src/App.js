caee2954daccbce84263564649fc661ec696b1aa
Asif khan <asifkhan@Asifs-MacBook-Air.local>
2024-07-31 17:43:21 +0530

    Initialized the project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      React Time chat Application now ready to contribute.....
+    </div>
+  );
+}
+
+export default App;
