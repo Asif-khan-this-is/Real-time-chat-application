a6dde49a0e42d561f874c07473091ad8ac28468f
Asif khan <asifkhan@Asifs-MacBook-Air.local>
2024-07-31 18:27:06 +0530

    created basic template and intialized all th e file and folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      React Time chat Application now ready to contribute.....
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Register.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="register">
+      <h2>Register</h2>
+      {/* Add registration form here */}
+    </div>
+  );
+};
+
+export default Register;
